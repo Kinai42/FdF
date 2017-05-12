b6df95777effd2a1e1768956306d2ff0bd4c384b
Damien BAUDUIN <dbauduin@e3r11p15.42.fr>
2017-05-12 04:22:02 +0200

    help

--- srcs/windows.c
+++ srcs/windows.c
@@ -6,7 +6,7 @@
 /*   By: dbauduin <dbauduin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 01:30:26 by dbauduin          #+#    #+#             */
-/*   Updated: 2017/05/12 04:03:26 by dbauduin         ###   ########.fr       */
+/*   Updated: 2017/05/12 04:19:53 by dbauduin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,10 @@ void		start_fdf(t_fdf *fdf, char *av)
 	{
 		//		print_map(fdf);
 		fdf->image = mlx_new_image(fdf->mlx, SCREEN_X, SCREEN_Y);
-		fdf->pixel = mlx_get_data_addr (fdf->image, &fdf->endian[0], &fdf->endian[1], &fdf->endian[2]);
+		fdf->pixel = mlx_get_data_addr(fdf->image, &fdf->endian[0], &fdf->endian[1], &fdf->endian[2]);
+		menu(fdf);
 		mlx_put_image_to_window(fdf->mlx, fdf->win, fdf->image, 0, 0);
 		//	mlx_key_hook(fdf->win, act_windows, fdf);
-		fdf->o_x = 10;
-		fdf->o_y = 1;
-		fdf->d_x = 10;
-		fdf->d_y = 10;
-		fdf->endian[0] = 32;
-		fdf->endian[1] = 4 * SCREEN_X;
-		fdf->endian[2] = 0;
-		draw_line(fdf, 255, 0, 0);
 		mlx_string_put(fdf->mlx, fdf->win, 2, 2, 0x00FF0000, "MENU");
 		mlx_string_put(fdf->mlx, fdf->win, 910, 970, 0x00FF0050, "DBAUDUIN");
 
@@ -44,7 +37,7 @@ void		start_fdf(t_fdf *fdf, char *av)
 }
 
 
-/*void	menu(t_fdf *fdf)
+void	menu(t_fdf *fdf)
 {
 
 	fdf->o_x = 10;
@@ -58,4 +51,4 @@ void		start_fdf(t_fdf *fdf, char *av)
 	mlx_string_put(fdf->mlx, fdf->win, 2, 2, 0x00FF0000, "MENU");
 	mlx_string_put(fdf->mlx, fdf->win, 910, 970, 0x00FF0050, "DBAUDUIN");
 
-}*/
+}
